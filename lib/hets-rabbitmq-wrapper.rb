c7b1f7bb7c5a82dd98855f6a404f644e391e3591
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-09-01 19:03:01 +0200

    Add basic bunny message receiver.

--- lib/hets-rabbitmq-wrapper.rb
+++ lib/hets-rabbitmq-wrapper.rb
@@ -1,5 +1,19 @@
-require "hets-rabbitmq-wrapper/version"
+require 'hets-rabbitmq-wrapper/version'
+require 'bunny'
 
 module HetsRabbitMQWrapper
-  # Your code goes here...
+  def self.run
+    connection = Bunny.new
+    connection.start
+    channel = connection.create_channel
+    #worker will only accept one message at time
+    channel.prefetch(1)
+    request_queue = channel.queue('hets-request', durable: true, auto_delete: false)
+    result_queue = channel.queue('hets-result')
+
+    request_queue.subscribe(manual_ack: true, block: true, timeout: 0) do |delivery_info, properties, body|
+      #todo: push body to hets
+      channel.ack(delivery_info.delivery_tag)
+    end
+  end
 end
