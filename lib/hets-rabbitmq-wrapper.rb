ad0aa2e0828a953f4a9d4d005edfdc03903d46ee
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-09-12 14:58:05 +0200

    Add queues and subscriptions for minimal and parsing version.

--- lib/hets-rabbitmq-wrapper.rb
+++ lib/hets-rabbitmq-wrapper.rb
@@ -7,18 +7,42 @@ module HetsRabbitMQWrapper
   def self.run
     connection = Bunny.new
     connection.start
-    channel = connection.create_channel
-    # worker will only accept one message at time
-    channel.prefetch(1)
-    request_queue =
-      channel.queue('hets-request', durable: true, auto_delete: false)
-    result_queue = channel.queue('hets-result')
 
-    request_queue.subscribe(manual_ack: true,
-                            block: true,
-                            timeout: 0) do |delivery_info, _properties, _body|
-      # TODO: push body to hets
-      channel.ack(delivery_info.delivery_tag)
+    min_parsing_version(connection)
+  end
+
+  def self.instance_version
+    #get version from HetsInstance
+  end
+
+  def self.min_parsing_version(connection)
+    channel = connection.create_channel
+    channel.exchange_declare('ex_min_parsing_version', 'x-recent-history',
+                             {'x-recent-history-length' => 1})
+    q_min_parsing_version = channel.queue('q_min_parsing_version',
+                                          durable: true, auto_delete: false)
+    q_min_parsing_version.bind('ex_min_parsing_version')
+    q_min_parsing_version.
+      subscribe(block: false,
+                timeout: 0) do |delivery_info, properties, version|
+      subscribe(version, connection)
+    end
+  end
+
+  def self.subscribe(version, connection)
+    channel = connection.create_channel
+    channel.prefetch(1)
+    queues = Hash.new
+    queues["#{version}"] = channel.queue("parsing-version-#{version}",
+                                         auto_delete: true)
+    puts queues["#{version}"]
+    if version.to_i <= instance_version.to_i
+      queues["#{version}"].
+        subscribe(block: false, manual_ack: true,
+                  timeout: 0) do |delivery_info, properties, body|
+        channel.ack(delivery_info.delivery_tag)
+        #push body to hets
+      end
     end
   end
 end
