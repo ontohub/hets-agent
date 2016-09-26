71147caf621a9c1c552f3d922f1fced1bfc8c6cb
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-09-26 09:52:31 +0200

    Add first rspec bunnymock test and config.

--- spec/hets-rabbitmq-wrapper_spec.rb
+++ spec/hets-rabbitmq-wrapper_spec.rb
@@ -7,3 +7,12 @@ describe HetsRabbitMQWrapper do
     expect(HetsRabbitMQWrapper::VERSION).not_to be nil
   end
 end
+
+describe HetsRabbitMQWrapper::Subscriber do
+  context 'minimal parsing version queue' do
+    it 'subscribe to' do
+      queue = HetsRabbitMQWrapper::Subscriber.new(BunnyMock.new).min_parsing_version_queue
+      expect(session.queue_exists?('q_min_parsing_version')).to be_truthy
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'hets-rabbitmq-wrapper'
 require_relative 'support/simplecov'
+require 'bunny-mock'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    HetsRabbitMQWrapper::Subscriber.new(BunnyMock.new)
+  end
+end
