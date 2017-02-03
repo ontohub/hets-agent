f9f5abcff223081439484390bf2a59659757a01c
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-02-03 11:39:48 +0100

    Support and test XRecentHistoryExchange

--- spec/hets-rabbitmq-wrapper_spec.rb
+++ spec/hets-rabbitmq-wrapper_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'support/bunnymock_recent_history_exchange'
+require 'pry'
 
 describe HetsRabbitMQWrapper do
   it 'has a version number' do
@@ -11,7 +13,9 @@ end
 describe HetsRabbitMQWrapper::Subscriber do
   context 'minimal parsing version queue' do
     it 'subscribe to' do
-      queue = HetsRabbitMQWrapper::Subscriber.new(BunnyMock.new).min_parsing_version_queue
+      queue = HetsRabbitMQWrapper::Subscriber.new(BunnyMock.new).
+        send(:min_parsing_version_queue)
+      session = queue.channel.connection
       expect(session.queue_exists?('q_min_parsing_version')).to be_truthy
     end
   end
