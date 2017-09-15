8e16f060c5b057d3e51859c04c344e897d33cc30
Tom Gehrke <phyrog@cognitive-coding.com>
2017-09-15 10:58:44 +0200

    Remove rubinius from travis builds

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-if RUBY_ENGINE == 'ruby' # not 'rbx'
-  unless defined?(Coveralls)
-    require 'simplecov'
-    require 'coveralls'
-    SimpleCov.formatters = [
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter,
-    ]
-    SimpleCov.start
-  end
+unless defined?(Coveralls)
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
+  ]
+  SimpleCov.start
 end
