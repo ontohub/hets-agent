aedf9bca1e5d0cbe34a77bcf34691a142cdd1a95
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-09-29 12:38:59 +0200

    Fix implementation and specs of subscriber.

--- lib/hets-agent/subscriber.rb
+++ lib/hets-agent/subscriber.rb
@@ -16,7 +16,7 @@ module HetsAgent
 
     def call
       @connection.start
-      subscribe_min_parsing_version_queue
+      subscribe_version_requirement_queue
     ensure
       @connection.close
     end
@@ -29,10 +29,10 @@ module HetsAgent
     private
 
     # Binds queue to exchange and subscribes to mininmal parsing version queue
-    def subscribe_min_parsing_version_queue
-      q_min_parsing_version = min_parsing_version_queue
-      q_min_parsing_version.bind('ex_min_parsing_version')
-      q_min_parsing_version.
+    def subscribe_version_requirement_queue
+      q_version_requirement = version_requirement_queue
+      q_version_requirement.bind('ex_hets_version_requirement')
+      q_version_requirement.
         subscribe(block: true,
                   timeout: 0) do |_delivery_info, _properties, requirement|
         subscribe_worker_queue(requirement)
@@ -40,25 +40,26 @@ module HetsAgent
     end
 
     # Creates an exchange and a queue for minimal parsing version
-    def min_parsing_version_queue
+    def version_requirement_queue
       channel = @connection.create_channel
-      channel.exchange('ex_min_parsing_version',
+      channel.exchange('ex_hets_version_requirement',
                        type: 'x-recent-history',
                        arguments: {'x-recent-history-length' => 1})
-      channel.queue('q_min_parsing_version', durable: true, auto_delete: false)
+      channel.queue('q_hets_version_requirement',
+                    durable: true,
+                    auto_delete: false)
     end
 
     # Subscribes to worker queue if min version is <= own version
     def subscribe_worker_queue(requirement)
-      queues = create_worker_queue(requirement)
       unless Gem::Requirement.new(*requirement.split(',')).
           satisfied_by?(Gem::Version.new(hets_version))
         return
       end
-      queues.
-        subscribe(block: false, manual_ack: true,
-                  timeout: 0) do |delivery_info, _properties, _body|
-        queues.channel.acknowledge(delivery_info.delivery_tag)
+      queue = create_worker_queue(requirement)
+      queue.subscribe(block: false, manual_ack: true,
+                      timeout: 0) do |delivery_info, _properties, _body|
+        queue.channel.acknowledge(delivery_info.delivery_tag)
         # TODO: push body to hets
       end
     end
@@ -67,7 +68,7 @@ module HetsAgent
     def create_worker_queue(requirement)
       channel = @connection.create_channel
       channel.prefetch(1)
-      channel.queue("parsing-version-#{requirement}", auto_delete: true)
+      channel.queue("hets-#{requirement}", auto_delete: true)
     end
   end
 end
--- spec/lib/subscriber_spec.rb
+++ spec/lib/subscriber_spec.rb
@@ -3,12 +3,16 @@
 require 'spec_helper'
 
 describe HetsAgent::Subscriber do
+  before do
+    HetsAgent::Application.boot
+  end
+
   subject { HetsAgent::Subscriber.new(BunnyMock.new) }
   let(:queue) { subject.call }
   let(:session) { queue.channel.connection }
   let(:version_requirement) { '~> 0.1.0' }
-  let(:version) { '0.1.1' }
-  let(:queue_name) { "parsing-version-#{version}" }
+  let(:version) { '0.1.5' }
+  let(:queue_name) { "hets-#{version_requirement}" }
 
   before do
     allow_any_instance_of(HetsAgent::Hets::VersionCaller).
@@ -24,23 +28,31 @@ describe HetsAgent::Subscriber do
 
   context 'minimal parsing version queue' do
     it 'subscribe to' do
-      expect(session.queue_exists?('q_min_parsing_version')).to be_truthy
+      expect(session.queue_exists?('q_hets_version_requirement')).to be(true)
     end
   end
 
   context 'worker queue' do
     before do
-      allow(subject).
-        to receive(:call_hets_version).and_return(version)
-      queue.publish(version)
+      allow(subject).to receive(:call_hets_version).and_return(version)
+      queue.publish(version_requirement)
     end
 
-    it 'subscribes successfully' do
-      expect(session.queue_exists?(queue_name)).to be(true)
+    context 'satisfying the version requirement' do
+      it 'subscribes successfully' do
+        expect(session.queue_exists?(queue_name)).to be(true)
+      end
+
+      it 'receives message' do
+        session.queues[queue_name].publish('Foobar')
+      end
     end
 
-    it 'receives message' do
-      session.queues[queue_name].publish('Foobar')
+    context 'not satisfying the version requirement' do
+      let(:version_requirement) { '~> 0.1.0, < 0.1.1' }
+      it 'does not subscribe' do
+        expect(session.queue_exists?(queue_name)).to be(false)
+      end
     end
   end
 end
