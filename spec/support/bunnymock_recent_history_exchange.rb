747f5ddbf181b7f68ab8620fec6fe19c606d3e6e
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-02-19 08:30:22 +0100

    Simplify rubocop config. (#64)

--- spec/support/bunnymock_recent_history_exchange.rb
+++ spec/support/bunnymock_recent_history_exchange.rb
@@ -3,6 +3,7 @@
 require 'bunny-mock'
 
 module BunnyMock
+  # Mock the recent history exchange extension of RabbitMQ.
   class Exchange
     # Monkeypatch, XRecentHistory be found by the ex declare "x-recent-history"
     def self.declare(channel, name = '', **opts)
@@ -16,6 +17,7 @@ module BunnyMock
     end
   end
 
+  # Mock the recent history exchange extension of RabbitMQ.
   module Exchanges
     class XRecentHistory < BunnyMock::Exchanges::Fanout
     end
